38991d4d67ff656e28c4c77f3d00f377a9838ef2
Alexey-Sagaydak <99820328+Alexey-Sagaydak@users.noreply.github.com>
2023-02-12 14:07:06 +0700

    add Page

---
+++ VirtualArray/IPage.cs
@@ -0,0 +1,11 @@
+﻿namespace VirtualArray
+{
+    public interface IPage
+    {
+        int[] Array { get; set; }
+        IBitMap BitMap { get; set; }
+        byte IsModified { get; set; }
+        DateTime LastCall { get; set; }
+        long Number { get; set; }
+    }
+}
---
+++ VirtualArray/Page.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualArray
+{
+    public class Page : IPage
+    {
+        private long number;
+        public long Number
+        {
+            get { return number; }
+            set
+            {
+                if (value < 0) throw new ArgumentException("Number must be positive");
+                number = value;
+            }
+        }
+
+        private byte isModified;
+        public byte IsModified
+        {
+            get { return isModified; }
+            set
+            {
+                if (!(value == 0 || value == 1)) throw new ArgumentException("Value must be 0 ot 1");
+                isModified = value;
+            }
+        }
+
+        public DateTime LastCall { get; set; }
+        public IBitMap BitMap { get; set; }
+        public int[] Array { get; set; }
+
+        public Page(int Length, int Number, DateTime LastCall = new DateTime(), byte IsModified = 0)
+        {
+            if (Length < 0) throw new ArgumentException("Length must not be negative");
+
+            Array = new int[Length];
+            BitMap = new BitMap(Length);
+            this.Number = Number;
+            this.IsModified = IsModified;
+            this.LastCall = LastCall;
+        }
+    }
+}
