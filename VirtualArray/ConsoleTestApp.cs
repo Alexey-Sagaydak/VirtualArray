d0d58b281edbe1b5f8f17ac5440a08087e2eb0ea
Alexey-Sagaydak <99820328+Alexey-Sagaydak@users.noreply.github.com>
2023-02-15 02:58:49 +0700

    add test script

--- VirtualArray/ConsoleTestApp.cs
+++ VirtualArray/ConsoleTestApp.cs
@@ -10,7 +10,69 @@ namespace VirtualArray
     {
         public static void Run()
         {
+            IVirtualArray arr = null;
+            try
+            {
+                string filename;
+                int numberOfPages, pageCapacity;
 
+                Console.Write("Array length: ");
+                int length = int.Parse(Console.ReadLine());
+
+                Console.Write("Use default settings? (1, 0): ");
+                if (Console.ReadLine() == "1")
+                {
+                    arr = new VirtualArray(length);
+                }
+                else
+                {
+                    Console.Write("Filename: ");
+                    filename = Console.ReadLine();
+                    Console.Write("Number of pages: ");
+                    numberOfPages = int.Parse(Console.ReadLine());
+                    Console.Write("Page capacity: ");
+                    pageCapacity = int.Parse(Console.ReadLine());
+
+                    arr = new VirtualArray(length, numberOfPages, pageCapacity, filename);
+                }
+
+                Console.Clear();
+
+                bool flag = true;
+                while (flag)
+                {
+                    Console.WriteLine("MENU\n1 - Set value\n2 - Delete value\n3 - Save and exit");
+                    int index, value;
+
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.Write("Index: ");
+                            index = int.Parse(Console.ReadLine());
+                            Console.Write("Value: ");
+                            value = int.Parse(Console.ReadLine());
+                            arr[index] = value;
+                            break;
+
+                        case "2":
+                            Console.Write("Index: ");
+                            index = int.Parse(Console.ReadLine());
+                            arr.Delete(index);
+                            break;
+
+                        case "3":
+                            arr.Dispose();
+                            flag = false;
+                            break;
+                    }
+
+                    Console.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
